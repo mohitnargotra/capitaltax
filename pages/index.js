4d68485c2868b05ae690426b953f920716d817bc
Mohit Nargotra <narg0001@algonquinlive.com>
2024-02-19 20:04:06 -0500

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Wrapper from "../layout/wrapper";
+
+import ServiceV4 from "./pages-menu/service-v4";
+
+const MainRoot = () => {
+  return (
+    <Wrapper>
+      <ServiceV4 />
+    </Wrapper>
+  );
+};
+
+export default MainRoot;
